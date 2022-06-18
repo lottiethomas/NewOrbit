1e8933c6b7ef1fe2ee4dccdef5de4bee653fc05b
Lottie Thomas <lottiethomas94@hotmail.co.uk>
2022-06-18 18:04:14 +0100

    Add main class and POM

---
+++ src/main/java/Application.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright © 2022 Lottie Thomas
+ */
+
+/**
+ * Entry point for the application
+ *
+ * @author lottiethomas
+ */
+public final class Application
+{
+   /**
+    * Entry point to the application
+    * @param args Application arguments
+    */
+   public static void main( String[] args )
+   {
+      System.out.println( "Hello World!" ); // NON-NLS
+   }
+}
